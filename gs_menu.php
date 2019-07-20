7e2e6ad9853073d5a2305e6eef097c047bf5a50e
dataleake <volcoman234@aim.com>
2019-07-20 12:36:55 +0000

    Add menu and portal page that is unused to date

---
+++ gs_menu.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<p><b>MENU</b></p>
+	<a href = "/aws/gs_search.php">Product Search</a>
+        <a href = "/aws/gs_login.php">Employee Login</a>
+      <!-- This page is a practice template for redirecting people who arent logged in-->
+        <a href = "/aws/gs_updates.php">Inventory Updates</a>
+</head>
+
+</html>
---
+++ gs_portal.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+
+<head>
+
+		<title> Portal </title>
+	   <center><h2> Welcome <h2></center>
+
+</head>
+
+<body></body>
+
+
+</html>
